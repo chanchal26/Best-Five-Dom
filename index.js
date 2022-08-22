d19ed85f4bfba692c08d3a32c5221ae2fbdad45e
chanchal26 <chanchal.mch2613@gmail.com>
2022-08-22 17:12:50 +0600

    click after button disable done

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// const btn = document.getElementsByClassName("select-btn");
-// let playerField = document.getElementsByClassName("player-field");
-// function addtoev() {
-//     for (let i = 0; i < btn.length; i++) {
-//         btn[i].addEventListener("click", function () {
-//             let playerName = btn[i].parentElement.children[0].innerHTML;
-//             for (let c = 0; c < playerField.length; c++) {
-//                 playerField[c].innerHTML = playerName;
-//             }
-//         });
-//     }
-// }
-// window.addEventListener("load", function () {
-//     addtoev();
-// });
-
 const array = [];
 
 function display(playersField) {
@@ -34,18 +18,14 @@ function display(playersField) {
 function playerAdded(element) {
 
     const playerName = element.parentNode.children[0].innerText;
-
-
-    array.push(playerName);
-    if (array.length <= 5) {
-        document.getElementById("total-added-player").innerText = array.length;
-
+    if (array.length < 5) {
+        document.getElementById("total-added-player").innerText = array.length + 1;
+        array.push(playerName);
     } else {
 
         alert("You allready select 5 players");
     }
-
     display(array);
-    const button = document.getElementsByClassName("select-btn");
-    button.disable = true;
 }
+
+
